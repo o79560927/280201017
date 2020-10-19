1f6c14a71ae4c73a0919cce1a2a49e01ca860a98
o79560927 <cihatgelir@ogr365.iyte.edu.tr>
2020-10-19 09:25:34 +0000

    c

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,1 +1,18 @@
-print("hello, world!")
+
+"""
+a=2
+b=6
+c=-20
+nom=-b +(b**2 - 4*a*c)**0.5
+nom2=-b -(b**2 - 4*a*c)**0.5
+den = 2*a
+
+x1 = nom/den
+x2 =nom2/den
+print(x1, x2)
+
+"""
+temp = float(input("what is the temperature : "))
+temp2 = float(temp*1.8 +32)
+
+print(temp2)
