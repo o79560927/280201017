c5138dd93dc32011d0d66e0edea318cb16f05ef0
o79560927 <cihatgelir@ogr365.iyte.edu.tr>
2020-10-26 02:52:53 +0000

    1

--- asynchron_lectures/elements_of_programming.py
+++ asynchron_lectures/elements_of_programming.py
@@ -5,5 +5,33 @@ volume_of_sphere = 4/3*pi*r**3
 
 print(volume_of_sphere)
 ***********************************************
-"""
 
+price_book = 24.95
+discount = (24.95)*(40/100)
+copies = 60
+shipping_cost = 3 + (copies-1)*0.75
+total_cost = (price_book - discount)*copies + shipping_cost
+
+print("total cost is" , int(total_cost) , "$")
+
+***********************************************
+
+first_8_book = 1.0
+per_book_after_8 = 19.99
+total_spent = 80.96
+extra_books = (total_spent - first_8_book)/(per_book_after_8)
+print("You did", extra_books,"books purchase" )
+***********************************************
+hour = 6
+minute = 52
+run_time = 8*1 + 3*6 +2*8
+
+coming_time = minute + run_time
+
+while coming_time > 60 :
+  coming_time -= 60
+  hour +=1
+
+print(hour,".",coming_time)
+***********************************************
+"""
