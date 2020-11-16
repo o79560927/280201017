736cdd7d47223e16250e9ad382d8b78d898bd35f
o79560927 <cihatgelir@ogr365.iyte.edu.tr>
2020-11-16 09:13:00 +0000

    .

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,3 +1,13 @@
+"""
 inp = int(input("Enter an integer :"))
-for i in range(1,11):
-  print(inp , "x",  i , "=" , inp*i )
+for i in range(1, 11):
+    print(inp, "x", i, "=", inp * i)
+"""
+number = int(input("How many numbers will be :"))
+list = []
+for i in range(number):
+  input_ = int(input("Enter a number :"))
+  if input_%2 == 0:
+    list.append(input_)
+
+print((len(list)/number)*100)
