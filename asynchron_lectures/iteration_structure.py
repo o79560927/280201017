15e2ece73c0cd561011d81abefdbce863ca7ad3f
o79560927 <cihatgelir@ogr365.iyte.edu.tr>
2020-11-20 05:09:02 +0000

    .

--- asynchron_lectures/iteration_structure.py
+++ asynchron_lectures/iteration_structure.py
@@ -15,9 +15,8 @@ for i in range(1,6):
   print("\n")
 """
 #Trace Tables Example 2
-
+"""
 user_input = int(input("Your number :"))
-
 for i in range(2,user_input):
   if user_input % i == 0:
     print(False)
@@ -25,5 +24,47 @@ for i in range(2,user_input):
   i = i+1
 if i == (user_input-1):
   print(True)
-
-
+"""
+# Maximum number
+"""
+number_of_numbers = int(input("How many nmumber are there :"))
+values = []
+for i in range(number_of_numbers):
+  user_number = int(input("Your number :"))
+  values.append(user_number)
+print("Largest value of your numbers :" , max(values))
+"""
+#Exercise 1
+"""
+low  = int(input("Low  :"))
+high = int(input("High :"))
+for i in range(low,(high+1)):
+  print(i**2)
+"""
+#Exercise 2
+"""
+low  = int(input("Low  :"))
+high = int(input("High :"))
+summ = 0
+for i in range(low,(high+1)):
+  summ = summ + i
+print(summ)
+"""
+#Exercise 3
+"""
+low  = int(input("Low  :"))
+high = int(input("High :"))
+for i in range(low,(high+1)):
+  print(i)
+"""
+#Exercise 4
+"""
+low  = int(input("Low  :"))
+high = int(input("High :"))
+for i in range(low,(high+1)):
+  for k in range(2,i):
+    if i % k == 0:
+      break;
+  if k == (i-1):
+    print(i)
+"""
