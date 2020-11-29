adb6bafcf0166b7d6b3cf2fa3a46ad7c721fd43f
o79560927 <cihatgelir@ogr365.iyte.edu.tr>
2020-11-29 18:21:26 +0000

    .

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -30,11 +30,51 @@ for i in grades:
   avarage_grade = i[0]*0.3 + i[1]*0.3 + i[2]*0.4
   print("students grade =", avarage_grade) 
 """
+#example3
+"""
+#[5, 91, 55, 20, 69, 19, 5, 20, 69, 78, 78]
+#[91, 78, 69, 55, 20, 19, 5]
+item_number = int(input("What is the number of items? :"))
+user_item_list = []
+for i in range(item_number):
+  item = int(input("item:"))
+  if user_item_list.count(item) == 0:
+    user_item_list.append(item)
+user_item_list.sort(reverse = True)
+print(user_item_list)
 
-liste = []
-for i in range(999999):
-  a = random.randint(0,1)
-  liste.append(a)
+"""
+#example4
+'''
+input_number = int(input("Insert the number to build an identity matrix. "))
+matrix = []
+counter = 1
+for x in range(input_number) :
+  matrix = []
+  for y in range(1, input_number + 1) :
+    if y == counter :
+      matrix.append(1)
+    else :
+      matrix.append(0)
+  counter += 1
+  print(matrix)
+'''
 
-print('0:',liste.count(0))
-print('1:',liste.count(1))
+#example5
+'''
+n = int(input("Matrix number: "))
+string = ''
+sum_list = []
+
+for i in range(n):
+    for j in range(n):
+        number = int(input("Input number: "))
+
+        if i == j:
+            sum_list.append(number)
+sum = 0
+for i in sum_list:
+    sum += i
+
+print("Output: ", sum)
+'''
